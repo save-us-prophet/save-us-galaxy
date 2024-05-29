6bbb10151e752d7da61e37019abf5dd4f6556753
SANGWOO PARK <cyberprophet@saveus.it>
2024-05-29 18:56:10 +0900

    heart rate measured from Galaxy Fit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:save_us_galaxy/galaxy.dart';
 
 void main() {
@@ -45,6 +47,103 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _platformVersion = platformVersion;
     });
+    await startScan();
+  }
+
+  Future<void> startScan() async {
+    FlutterBluePlus.events.onMtuChanged.listen((event) async {
+      for (final ds in await event.device.discoverServices()) {
+        if (kDebugMode) {
+          print('serviceUuid:: ${ds.serviceUuid.str128}');
+        }
+        if (ds.serviceUuid == Guid('0000180D-0000-1000-8000-00805F9B34FB')) {
+          for (final dc in ds.characteristics) {
+            if (kDebugMode) {
+              print('characteristics:: ${dc.characteristicUuid.str128}');
+            }
+            if (dc.characteristicUuid ==
+                Guid('00002A37-0000-1000-8000-00805F9B34FB')) {
+              await dc.setNotifyValue(true);
+            }
+          }
+        }
+      }
+    });
+    FlutterBluePlus.events.onDiscoveredServices.listen((event) async {
+      if (kDebugMode) {
+        print('onDiscoveredServices:: ${event.services}');
+      }
+    });
+    FlutterBluePlus.events.onCharacteristicWritten.listen((event) {
+      if (kDebugMode) {
+        print('onCharacteristicWritten:: ${event.value}');
+      }
+    });
+    FlutterBluePlus.events.onDescriptorWritten.listen((event) {});
+    FlutterBluePlus.events.onConnectionStateChanged.listen((event) {});
+    FlutterBluePlus.events.onCharacteristicReceived.listen((event) {
+      if (kDebugMode) {
+        print('onCharacteristicReceived:: ${event.value}');
+      }
+    });
+    FlutterBluePlus.scanResults.listen((results) async {
+      for (ScanResult r in results) {
+        if (r.device.advName.startsWith('Galaxy')) {
+          await FlutterBluePlus.stopScan();
+
+          connectToDevice(r.device);
+
+          repeat = false;
+          break;
+        }
+        if (r.device.advName.isEmpty) {
+          continue;
+        }
+        if (devices.any((e) => e.advName == r.device.advName)) {
+          continue;
+        }
+        if (kDebugMode) {
+          print({
+            r.advertisementData.advName,
+            r.advertisementData.manufacturerData
+          });
+        }
+        devices.add(r.device);
+      }
+    });
+    final duration = Duration(seconds: 9);
+
+    while (repeat) {
+      await FlutterBluePlus.startScan(timeout: duration, withMsd: [
+        // SAMSUNG
+        MsdFilter(0x0075),
+
+        // Galaxy Fit3
+        MsdFilter(0x0075,
+            data: [1, 0, 2, 0, 1, 3, 255, 0, 0, 67, 42, 2, 10, 21, 232, 5])
+      ]);
+
+      await Future.delayed(duration);
+    }
+  }
+
+  void connectToDevice(BluetoothDevice device) async {
+    await device.connect();
+
+    setState(() {
+      connectedDevice = device;
+      _platformVersion = device.advName;
+    });
+  }
+
+  Future discoverServices() async {
+    if (connectedDevice == null) return;
+
+    List<BluetoothService> services = await connectedDevice!.discoverServices();
+
+    setState(() {
+      this.services = services;
+    });
   }
 
   @override
@@ -60,4 +159,11 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+
+  BluetoothDevice? connectedDevice;
+  List<BluetoothService> services = [];
+
+  List<BluetoothDevice> devices = [];
+
+  bool repeat = true;
 }
